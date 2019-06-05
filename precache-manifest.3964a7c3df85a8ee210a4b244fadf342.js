9da648aaa7bf96a4525210840559cc2026e02154
Trystan Eveno <trystan.eveno@lephare.com>
2019-06-05 03:41:30 +0200

    :rocket: deploy

---
+++ precache-manifest.3964a7c3df85a8ee210a4b244fadf342.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = [
+  {
+    "revision": "99486b04bd17ecd964f8483bdeb2767d",
+    "url": "sounds/PS4/Load_Error.mp3"
+  },
+  {
+    "revision": "e5d3c6e81c171468b143922e7ebe520e",
+    "url": "sounds/PS4/Menu_them_PS4_old.mp3"
+  },
+  {
+    "revision": "2dcb299b934d4cd809a1c3dc6cbabbbd",
+    "url": "sounds/PS4/Notification.mp3"
+  },
+  {
+    "revision": "c40a9a27f59cd173c363f7f1371bde9f",
+    "url": "sounds/PS4/Trophy.mp3"
+  },
+  {
+    "revision": "337f09299ad765af0d2b29924353d96c",
+    "url": "sounds/PS4/Turning_On.mp3"
+  },
+  {
+    "revision": "d634e4875d3abde6e7c30f7a56c9c82e",
+    "url": "sounds/PS4/Action_Up.mp3"
+  },
+  {
+    "revision": "3b9fc6381bc6707895bf",
+    "url": "js/about.663d5b62.js"
+  },
+  {
+    "revision": "f38925ebd7adf4b6bf4458a0ad83144e",
+    "url": "sounds/PS4/Intro_Start_Up_1.mp3"
+  },
+  {
+    "revision": "a9fc565972b3adc465850f9f786b4b37",
+    "url": "sounds/PS4/Intro_Start_Up_2.mp3"
+  },
+  {
+    "revision": "e525d38ef92a0ea6a106e29c09fda455",
+    "url": "sounds/PS4/Log_Off.mp3"
+  },
+  {
+    "revision": "0ecd52e7a11577e4d530",
+    "url": "js/app.3ccf0e4f.js"
+  },
+  {
+    "revision": "7277b3bc704d8c8a43586d737093b5d5",
+    "url": "sounds/PS4/Action_Down.mp3"
+  },
+  {
+    "revision": "31596b858ce4f7c32b40a64bd2ac8da1",
+    "url": "sounds/PS4/Action_Select.mp3"
+  },
+  {
+    "revision": "d97a4873c55214b854c4be2eaf02328c",
+    "url": "sounds/PS4/Action_Cancel.mp3"
+  },
+  {
+    "revision": "a334fd64e1d9141b81fe28b568ceb41a",
+    "url": "sounds/PS4/Action_Error.mp3"
+  },
+  {
+    "revision": "409ed429b0e55b640f9e8c4115541e6c",
+    "url": "sounds/Outlast/Switch_On_Camera.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "5dc85c7ff0ac4254a4200a0cabf5036b",
+    "url": "sounds/PS4/Action_Move.mp3"
+  },
+  {
+    "revision": "3816ba2d427ad9d24a64",
+    "url": "js/chunk-vendors.153b5e2a.js"
+  },
+  {
+    "revision": "71152b50fee7f3637128798ff2480888",
+    "url": "sounds/Outlast/Put_Battery.mp3"
+  },
+  {
+    "revision": "b661c28b0f28606a96722ad2d9588b70",
+    "url": "fonts/MaterialIcons-Regular.b661c28b.eot"
+  },
+  {
+    "revision": "b8fc9d96f846353cfb01a1264589a75a",
+    "url": "img/cabin-taken.png"
+  },
+  {
+    "revision": "1a7c2d99d55dec80efea6bb5c9960f10",
+    "url": "img/pedestrian.png"
+  },
+  {
+    "revision": "17f862940795a8c0278841ffca339d5d",
+    "url": "img/helper.png"
+  },
+  {
+    "revision": "6602a7acf89c6c5092ca018f0f5a2413",
+    "url": "img/cabin.png"
+  },
+  {
+    "revision": "fe2b30cb8aa164e2b7216b41ff1612fe",
+    "url": "img/sync.svg"
+  },
+  {
+    "revision": "10741e416a7dc5a41b1b1f347dc775b7",
+    "url": "index.html"
+  },
+  {
+    "revision": "521ebae9bd1158ae614382d77d4280f8",
+    "url": "fonts/SST-Medium.ttf"
+  },
+  {
+    "revision": "77a0ddf57e5308d1c2c6589e6f20ad55",
+    "url": "fonts/SST-Light.77a0ddf5.ttf"
+  },
+  {
+    "revision": "77a0ddf57e5308d1c2c6589e6f20ad55",
+    "url": "fonts/SST-Light.ttf"
+  },
+  {
+    "revision": "26207c293866e229a813eb12923d95fb",
+    "url": "fonts/SST-Roman.ttf"
+  },
+  {
+    "revision": "75259501f815377b53b92bfff10f05cf",
+    "url": "fonts/SST-Ultra-Light-Italic.ttf"
+  },
+  {
+    "revision": "453a91f6e15f556e856a706ac0bf3d48",
+    "url": "fonts/SST-Typewriter-Bold.ttf"
+  },
+  {
+    "revision": "a76799b72d9e76aa36ec882315c12682",
+    "url": "fonts/SST-Typewriter.ttf"
+  },
+  {
+    "revision": "358cc61c014b83c8d0208b5e62e251ad",
+    "url": "fonts/SST-Ultra-Light.ttf"
+  },
+  {
+    "revision": "5976036e3c27d9608993f0638a102da6",
+    "url": "fonts/SST-Medium-Italic.ttf"
+  },
+  {
+    "revision": "3a331f8961a1787b3f104d5dfc1426c5",
+    "url": "fonts/SST-Bold-Italic.ttf"
+  },
+  {
+    "revision": "436390eb091d19924b4ca2aa3fd989d4",
+    "url": "fonts/SST-Condensed-Bold.ttf"
+  },
+  {
+    "revision": "eb6fa924011a00487207c61a8819edf1",
+    "url": "fonts/SST-Heavy.ttf"
+  },
+  {
+    "revision": "2fc21bb2a758202c952acdf67f8d3be4",
+    "url": "fonts/SST-Heavy-Italic.ttf"
+  },
+  {
+    "revision": "072039b80b46f7d5d4d40f2bd8b81200",
+    "url": "fonts/SST-Condensed.ttf"
+  },
+  {
+    "revision": "7c3b1ce704861e052f7f9b5e98bea40f",
+    "url": "fonts/SST-Italic.ttf"
+  },
+  {
+    "revision": "a63abed95dbb4802d6f9c711a8c4c418",
+    "url": "fonts/SST-Light-Italic.ttf"
+  },
+  {
+    "revision": "64f42be7b4d097a3807fab68c42f2b33",
+    "url": "fonts/SST-Condensed-Medium.ttf"
+  },
+  {
+    "revision": "bafc913eb8cf3b14dae9dbfff5791ced",
+    "url": "fonts/SST-Bold.ttf"
+  },
+  {
+    "revision": "bca3a1873ac988faff0817eca96b2d86",
+    "url": "fonts/MaterialIcons-Regular.bca3a187.woff2"
+  },
+  {
+    "revision": "9219a80f0478e0bfdee5f4c753ce8535",
+    "url": "fonts/MaterialIcons-Regular.9219a80f.woff"
+  },
+  {
+    "revision": "586090b38a233ce0201fb221eb117a36",
+    "url": "fonts/MaterialIcons-Regular.586090b3.ttf"
+  },
+  {
+    "revision": "3816ba2d427ad9d24a64",
+    "url": "css/chunk-vendors.c30706a0.css"
+  },
+  {
+    "revision": "0ecd52e7a11577e4d530",
+    "url": "css/app.abe1c121.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.886c29053abe5c398bdb4bca41df4014.js"
+  "precache-manifest.3964a7c3df85a8ee210a4b244fadf342.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pwa-cabin-of-suicides"});
